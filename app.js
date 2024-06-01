c751d22a07a4e315f9266d3efaf36c40928fd35f
Vishesh Singh <VisheshSingh16@gmail.com>
2024-06-01 13:42:29 -0400

    Creating and injecting node long way

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const heading = React.createElement(
-  'h1', // element
-  { className: 'title' }, // Attributes
-  'Hello from React!' // Children
+// CREATE THE FOLLOWING HTML WITH REACT
+{
+  /* <div id='title'>
+    <div id='child'>
+        <h1 id="title"></h1>
+    </div>
+</div> */
+}
+
+const parentContainer = React.createElement(
+  'div', // element
+  { id: 'parent' }, // Attributes
+  React.createElement(
+    'div',
+    {
+      id: 'child',
+    },
+    React.createElement(
+      'h1',
+      {
+        id: 'title',
+      },
+      'Hello from React! '
+    )
+  ) // Children
 );
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(parentContainer);
